3691921c3387fedc68b3cfb921f7fdd0ad6067df
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-09-27 16:25:33 +0200

    Init welcome screen

---
+++ code/tictic/lib/constants/sizes.dart
@@ -0,0 +1,2 @@
+const kLogoSizeWelcome = 150.0;
+const kVerticalSpacing = 20.0;
---
+++ code/tictic/lib/screens/welcome.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:tictic/constants/sizes.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/back1.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                "assets/icons/logo.svg",
+                width: kLogoSizeWelcome,
+                height: kLogoSizeWelcome,
+              ),
+              SizedBox(height: kVerticalSpacing),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
