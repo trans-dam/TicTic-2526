65a131a571129aee1316ee08554ba2382277f07d
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-09-29 10:50:57 +0200

    Refactor SvgLogo widget to welcome screen

--- code/tictic/lib/screens/welcome.dart
+++ code/tictic/lib/screens/welcome.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tictic/constants/sizes.dart';
+import 'package:tictic/widgets/welcome/svg_logo.dart';
 import 'package:tictic/widgets/welcome/text_slider.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +21,7 @@ class WelcomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SvgPicture.asset(
-                "assets/icons/logo.svg",
-                width: kLogoSizeWelcome,
-                height: kLogoSizeWelcome,
-              ),
+              SvgLogo(),
               SizedBox(height: kVerticalPaddingXL),
               const TextSlider(),
             ],
---
+++ code/tictic/lib/widgets/welcome/svg_logo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../constants/sizes.dart';
+
+class SvgLogo extends StatelessWidget {
+  const SvgLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      "assets/icons/logo.svg",
+      width: kLogoSizeWelcome,
+      height: kLogoSizeWelcome,
+    );
+  }
+}
