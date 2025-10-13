0c91fcf8109ed9fcd046b6e605e38e97f9b08535
Daniel Schreurs <DanielSchreurs@users.noreply.github.com>
2025-10-13 10:31:11 +0200

    Add login screen with form validation and navigation to registration

--- code/tictic/lib/screens/welcome/widgets/action_buttons.dart
+++ code/tictic/lib/screens/welcome/widgets/action_buttons.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tictic/constants/fonts.dart';
 import 'package:tictic/constants/sizes.dart';
 import 'package:tictic/l10n/app_localizations.dart';
+import 'package:tictic/screens/login/login_screen.dart';
 import 'package:tictic/screens/register/register_screen.dart';
 
 class ActionButtons extends StatelessWidget {
@@ -17,7 +18,9 @@ class ActionButtons extends StatelessWidget {
             padding: const EdgeInsets.only(left: kHorizontalPadding),
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(textStyle: kLabelStyle),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.routName);
+              },
               child: Text(AppLocalizations.of(context)!.login),
             ),
           ),
